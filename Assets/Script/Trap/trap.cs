b2b0d14ec9834ba2edd451502dc55f3879a999e5
DESKTOP-QVOJ483\iibi203 <mnbvcxz195@naver.com>
2023-02-08 19:19:55 +0900

    돌 함정 기능 수정

--- Assets/Script/Trap/trap.cs
+++ Assets/Script/Trap/trap.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class trap : MonoBehaviour
 {
+    private WaitForSecondsRealtime waitForSeconds_CreateRock = new WaitForSecondsRealtime(1.5f);
+
     private RollingRockMemoryPool rollingRockMemoryPool;  //돌 생성 후 활성/비활성 관리
 
-    [Header("Spawn Points")]
+    [Header("PlayerPos")]
     [SerializeField]
-    private Transform rockSpawnPoint;        //돌 생성 위치
+    private Transform player;                             //플레이어 위치
+
+    [Header("Rolling Rock")]
     [SerializeField]
-    private Transform rockEndPoint;        //돌 마지막 위치
+    private Transform rockSpawnPoint;                     //돌 생성 위치
+    [SerializeField]
+    private Transform rockEndPoint;                       //돌 마지막 위치
+    [SerializeField]
+    private Transform rockActivePoint;                    //돌 함정 발동 위치(근처로 올때 함정 발동)
+    private float User_Rock_dis;                          //플레이어, 돌 함정 트리거 거리
+    [SerializeField]
+    private bool isStopableRock = false;                  //돌 함정 멈출 수 있는 스위치
+    [SerializeField]
+    private bool isActiveRock = false;                    //돌 함정 발동
+
 
 
     private void Awake()
@@ -20,16 +34,43 @@ public class trap : MonoBehaviour
 
     private void Update()
     {
-        
+        CheckCloseRockTrigger();
     }
 
     private void Start()
     {
-        //CreateRock();
+        StartCoroutine(CreateRock());
     }
 
-    public void CreateRock()
+/*    public void CreateRock()
     {
-        rollingRockMemoryPool.SpawnRock(rockSpawnPoint, rockEndPoint);
+        float distance = Vector3.Distance(player.position, rockActivePoint.position);
+        //Debug.Log(distance);
+
+        if(distance < 1.5f)
+            rollingRockMemoryPool.SpawnRock(rockSpawnPoint, rockEndPoint);
+    }*/
+
+    IEnumerator CreateRock()       //함정 발동 트리거 근처 올시 돌 함정 실행
+    {
+        while (true)
+        {
+            if (isActiveRock && !isStopableRock)
+            {
+                rollingRockMemoryPool.SpawnRock(rockSpawnPoint, rockEndPoint);
+            }
+
+            yield return waitForSeconds_CreateRock;
+        }
+    }
+    void CheckCloseRockTrigger()
+    {
+        User_Rock_dis = Vector3.Distance(player.position, rockActivePoint.position);
+
+        if (User_Rock_dis < 1.5f)
+        {
+            isActiveRock = true;
+        }
+
     }
 }
