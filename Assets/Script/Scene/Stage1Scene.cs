07a2844a9b7fca98609a3fddb3640b84a4998e6e
jinjin <maroon1566@gmail.com>
2023-02-20 11:39:56 +0900

    트랩 사운드

---
+++ Assets/Script/Scene/Stage1Scene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1Scene : MonoBehaviour
+{
+    [SerializeField] AudioSource bgm; 
+    void Start()
+    {
+        AudioManager.GetInstance().BgmPlay(bgm, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
