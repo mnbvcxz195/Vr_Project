9d6524fa2f11331dfb878fa1be4112fc5870c921
jinjin <maroon1566@gmail.com>
2023-02-21 12:47:26 +0900

    부셔지는 스크립트

---
+++ Assets/Breakable Medieval Containers/Script/jb_cont_break.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jb_cont_break : MonoBehaviour
+{
+
+	public Transform Fragments;
+	public float Spread, Force;
+	[SerializeField] GameObject Item;
+
+    private void Awake()
+    {
+		Item.gameObject.SetActive(false);
+
+	}
+	private void Start()
+    {
+
+	}
+    public void GrapoBj () 
+    {
+		Item.gameObject.SetActive(true);
+		Destroy(gameObject,0.5f);
+		Instantiate(Fragments, transform.position, transform.rotation);
+		Fragments.localScale = transform.localScale;
+		Vector3 breakPosition = transform.position;
+		Collider[] fragments = Physics.OverlapSphere(breakPosition, Spread);
+
+		foreach (Collider fragment in fragments)
+		{
+			if (fragment.attachedRigidbody)
+			{
+				fragment.attachedRigidbody.AddExplosionForce(Force, breakPosition, Spread);
+			}
+		}
+	}
+}
