19fbaacc6683d80620455e71d5ba7436e21c2952
JYS121206 <p.uncia1775@gmail.com>
2023-02-21 12:01:50 +0900

    튜토리얼 완료시 무너지는 맵 추가

--- Assets/Script/OnTriggerF.cs
+++ Assets/Script/OnTriggerF.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class OnTriggerF : MonoBehaviour
 {
     ScenesManager scenesManager;
+    public GameObject bossRoom1;
+    public GameObject bossRoom2;
 
     private void Start()
     {
@@ -16,12 +18,14 @@ public class OnTriggerF : MonoBehaviour
         if (other.tag == "Player")
         {
             Debug.Log($"튜토리얼이 종료되었습니다.");
-            Invoke("ToStage1", 5f);
+            Invoke("ToStage1", 1f);
         }
     }
 
     public void ToStage1()
     {
-        scenesManager.ChangeScene(Scene.Stage1);
+        bossRoom1.SetActive(false);
+        bossRoom2.SetActive(true);
+        //scenesManager.ChangeScene(Scene.Stage1);
     }
 }
